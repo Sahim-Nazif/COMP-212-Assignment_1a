681c7fe7e701f8c162ddb975defa50ed74101704
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-15 20:42:24 -0500

    created a while loop

--- Assignment_1a/Program.cs
+++ Assignment_1a/Program.cs
@@ -9,11 +9,27 @@ namespace Assignment_1a
 {
     //declaring delegate
     public delegate void CalculateDel(double a, double b);
-    class Program
+    class Program:Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalculateDel addDel, subDel, multiDel, divDel;
+
+            addDel = new CalculateDel(Add);
+            subDel = new CalculateDel(Subtract);
+            multiDel = new CalculateDel(Multiply);
+            divDel = new CalculateDel(Divide);
+
+            double a = 0,b;
+
+            while (a != -1)
+            {
+                Console.WriteLine("\nWelcome Centennial College Multicast Delegate Application");
+                Console.WriteLine("\n Please enter the first double number or -1 to discontinue");
+                a = double.Parse(Console.ReadLine());
+
+
+            }
         }
     }
 }
