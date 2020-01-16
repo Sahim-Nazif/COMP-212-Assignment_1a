2c3bc20ee350b408b6195ba73b4a40693d6e2d6c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-15 20:09:32 -0500

    Add project files.

---
+++ Assignment_1a/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1a
+{
+   public class Calculation
+    {
+        public static void Add(double a, double b)
+        {
+            double result = a + b;
+            Console.WriteLine("The result of addition is : " + result );
+        }
+
+        public  static void Subtract (double a, double b)
+        {
+
+        }
+    }
+}
---
+++ Assignment_1a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+/*
+ * Assignment 1a 
+ * COMP 212
+ * Winter 2020
+ * Multicast Delegate
+ */
+namespace Assignment_1a
+{
+    //declaring delegate
+    public delegate void CalculateDel(double a, double b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
